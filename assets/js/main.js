e035bee5a483dec183156f7b8d7473f753d1a824
Marchell Kevandra <117437628+mkxchl@users.noreply.github.com>
2024-10-23 18:17:21 +0700

    Encrypt Java

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,92 +1,244 @@
-/*=============== FILTERS TABS ===============*/
-const VanzXCard = document.querySelector(".VanzXCard");
-const XAbout = document.getElementById("#XAbout");
-const XActivity = document.getElementById("#Activity");
-const XConnect = document.getElementById("#Connect");
-const VanzMenu = document.querySelector(".VanzMenu");
-const VanzGame = document.querySelector(".VanzGame");
-const VanzMenuOverlay = document.querySelector(".VanzMenuOverlay");
-const VanzMusicOverlay = document.querySelector(".VanzMusicOverlay");
-const VanzGameOverlay = document.querySelector(".VanzGameOverlay");
-const VanzMusic = document.querySelector(".VanzMusic");
-
-const tabs = document.querySelectorAll("[data-target]"),
-  tabContents = document.querySelectorAll("[data-content]");
-
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    const target = document.querySelector(tab.dataset.target);
-
-    tabContents.forEach((tc) => {
-      klik.play();
-      tc.classList.remove("FiltersActive");
-    });
-    target.classList.add("FiltersActive");
-
-    tabs.forEach((t) => {
-      klik.play();
-      t.classList.remove("VanzBtnActive");
-    });
-    tab.classList.add("VanzBtnActive");
-    klik.play();
-  });
-});
-
-function OpenMenu() {
-  VanzMenu.classList.add("VanzMenuShow");
-  VanzXCard.style.overflow = "hidden";
-  klik.play();
+const _0x2cdba3 = _0x4a95
+;(function (_0x4690ce, _0x52ffaf) {
+  const _0x508c2e = _0x4a95,
+    _0x200d9f = _0x4690ce()
+  while (!![]) {
+    try {
+      const _0x473498 =
+        -parseInt(_0x508c2e(0x16e)) / 0x1 +
+        (-parseInt(_0x508c2e(0x144)) / 0x2) *
+          (-parseInt(_0x508c2e(0x146)) / 0x3) +
+        -parseInt(_0x508c2e(0x13c)) / 0x4 +
+        (parseInt(_0x508c2e(0x13e)) / 0x5) *
+          (parseInt(_0x508c2e(0x142)) / 0x6) +
+        (-parseInt(_0x508c2e(0x13a)) / 0x7) *
+          (-parseInt(_0x508c2e(0x157)) / 0x8) +
+        (parseInt(_0x508c2e(0x147)) / 0x9) *
+          (parseInt(_0x508c2e(0x167)) / 0xa) +
+        parseInt(_0x508c2e(0x150)) / 0xb
+      if (_0x473498 === _0x52ffaf) break
+      else _0x200d9f['push'](_0x200d9f['shift']())
+    } catch (_0x27e842) {
+      _0x200d9f['push'](_0x200d9f['shift']())
+    }
+  }
+})(_0x2f50, 0x337e4)
+function _0x2f50 () {
+  const _0x340d70 = [
+    '23163qehbGS',
+    '27nZRDTX',
+    'https://open.spotify.com/user/31ubcol5qj4y6mu3ifoxqbjgzpma?si=c48b3b26de694129',
+    'loop',
+    'classList',
+    '.snowflakes',
+    'overflow',
+    'https://tiktok.com/@mkxchl',
+    'VanzGameShow',
+    'https://open.spotify.com/playlist/1ZaVZkzQxjsSPUSnLDrL27?si=8580e4c5b3be448d',
+    '2198493ZFTEmo',
+    'VanzBtnActive',
+    'bx\x20bx-volume-mute',
+    'target',
+    'querySelector',
+    'add',
+    '[data-content]',
+    '304hiGPdV',
+    'VanzMenuShow',
+    'body',
+    'FiltersActive',
+    'style',
+    'querySelectorAll',
+    'location',
+    '#MusicA',
+    '.VanzMenuOverlay',
+    'dataset',
+    '.VanzMenu',
+    'DarkVanz',
+    '#Connect',
+    'remove',
+    'https://editor-miring-squad.netlify.app/mix/mk-song-1.mp3',
+    'animationDelay',
+    '510500eiyEnk',
+    'SnowShow',
+    'https://instagram.com/marchell_kevandra',
+    'click',
+    'hidden',
+    'https://steamcommunity.com/profiles/76561198858660119/',
+    'VanzXNavMusicShow',
+    '410703NXLKjp',
+    '.VanzGame',
+    'className',
+    'play',
+    'galery/',
+    'auto',
+    'random',
+    'forEach',
+    'animationDuration',
+    'https://editor-miring-squad.netlify.app/am/mix/klik.mp3',
+    'VanzMusicShow',
+    '.VanzXCard',
+    '#Activity',
+    'addEventListener',
+    'replace',
+    '40159eZyFQs',
+    'left',
+    '480108SrzMeV',
+    'autoplay',
+    '15dXiGUC',
+    '.VanzAlert',
+    '#XAbout',
+    '.snowflake',
+    '325812krDDcO',
+    '.IconMusic',
+    '2LhzoUY',
+    'getElementById'
+  ]
+  _0x2f50 = function () {
+    return _0x340d70
+  }
+  return _0x2f50()
 }
-function CloseOverlay() {
-  klik.play();
-  VanzXCard.style.overflow = "auto";
-  VanzMenu.classList.remove("VanzMenuShow");
-  VanzMusic.classList.remove("VanzMusicShow");
-  VanzGame.classList.remove("VanzGameShow");
+const VanzXCard = document[_0x2cdba3(0x154)](_0x2cdba3(0x179)),
+  BodyX = document[_0x2cdba3(0x154)](_0x2cdba3(0x159)),
+  XAbout = document[_0x2cdba3(0x145)](_0x2cdba3(0x140)),
+  XActivity = document[_0x2cdba3(0x145)](_0x2cdba3(0x137)),
+  XConnect = document[_0x2cdba3(0x145)](_0x2cdba3(0x163)),
+  VanzMenu = document[_0x2cdba3(0x154)](_0x2cdba3(0x161)),
+  VanzGame = document[_0x2cdba3(0x154)](_0x2cdba3(0x16f)),
+  VanzMenuOverlay = document[_0x2cdba3(0x154)](_0x2cdba3(0x15f)),
+  VanzMusicOverlay = document[_0x2cdba3(0x154)]('.VanzMusicOverlay'),
+  VanzGameOverlay = document['querySelector']('.VanzGameOverlay'),
+  VanzMusic = document['querySelector']('.VanzMusic'),
+  VanzAlert = document['querySelector'](_0x2cdba3(0x13f)),
+  VanzXNavMusic = document[_0x2cdba3(0x154)]('.VanzXNavMusic'),
+  MusicA = document[_0x2cdba3(0x145)](_0x2cdba3(0x15e)),
+  PictVanz = document[_0x2cdba3(0x145)]('#PictVanz'),
+  FullVolume = 'bx\x20bx-volume-full',
+  MuteVolume = _0x2cdba3(0x152),
+  IconMusic = document[_0x2cdba3(0x154)](_0x2cdba3(0x143)),
+  snowflakesSnow = document[_0x2cdba3(0x154)](_0x2cdba3(0x14b)),
+  snowflakes = document[_0x2cdba3(0x15c)](_0x2cdba3(0x141)),
+  tabs = document[_0x2cdba3(0x15c)]('[data-target]'),
+  tabContents = document[_0x2cdba3(0x15c)](_0x2cdba3(0x156))
+tabs['forEach'](_0x3b4414 => {
+  const _0x498fbf = _0x2cdba3
+  _0x3b4414[_0x498fbf(0x138)](_0x498fbf(0x16a), () => {
+    const _0x3e1093 = _0x498fbf,
+      _0x1f655b = document[_0x3e1093(0x154)](
+        _0x3b4414[_0x3e1093(0x160)][_0x3e1093(0x153)]
+      )
+    tabContents[_0x3e1093(0x175)](_0x106dbd => {
+      const _0x13177a = _0x3e1093
+      klik[_0x13177a(0x171)](),
+        _0x106dbd['classList'][_0x13177a(0x164)](_0x13177a(0x15a))
+    }),
+      _0x1f655b[_0x3e1093(0x14a)]['add'](_0x3e1093(0x15a)),
+      tabs[_0x3e1093(0x175)](_0x550365 => {
+        const _0x3ca9a1 = _0x3e1093
+        klik[_0x3ca9a1(0x171)](),
+          _0x550365[_0x3ca9a1(0x14a)][_0x3ca9a1(0x164)](_0x3ca9a1(0x151))
+      }),
+      _0x3b4414[_0x3e1093(0x14a)][_0x3e1093(0x155)](_0x3e1093(0x151)),
+      klik[_0x3e1093(0x171)]()
+  })
+})
+function OpenMenu () {
+  const _0x183deb = _0x2cdba3
+  VanzMenu[_0x183deb(0x14a)][_0x183deb(0x155)](_0x183deb(0x158)),
+    (VanzXCard['style'][_0x183deb(0x14c)] = _0x183deb(0x16b)),
+    klik[_0x183deb(0x171)]()
 }
-function OpenMusic() {
-  klik.play();
-  VanzMusic.classList.add("VanzMusicShow");
-  VanzMenu.classList.remove("VanzMenuShow");
-  VanzGame.classList.remove("VanzGameShow");
+function CloseOverlay () {
+  const _0x11ea0d = _0x2cdba3
+  klik[_0x11ea0d(0x171)](),
+    (VanzXCard[_0x11ea0d(0x15b)][_0x11ea0d(0x14c)] = _0x11ea0d(0x173)),
+    VanzMenu[_0x11ea0d(0x14a)][_0x11ea0d(0x164)](_0x11ea0d(0x158)),
+    VanzMusic[_0x11ea0d(0x14a)][_0x11ea0d(0x164)](_0x11ea0d(0x178)),
+    VanzGame[_0x11ea0d(0x14a)][_0x11ea0d(0x164)](_0x11ea0d(0x14e))
 }
-function OpenGame() {
-  klik.play();
-  VanzMenu.classList.remove("VanzMenuShow");
-  VanzGame.classList.add("VanzGameShow");
+function OpenMusic () {
+  const _0x418cbc = _0x2cdba3
+  klik[_0x418cbc(0x171)](),
+    VanzMusic[_0x418cbc(0x14a)]['add'](_0x418cbc(0x178)),
+    VanzMenu[_0x418cbc(0x14a)][_0x418cbc(0x164)](_0x418cbc(0x158)),
+    VanzGame[_0x418cbc(0x14a)][_0x418cbc(0x164)]('VanzGameShow')
 }
-function OpenGalery() {
-  klik.play();
-  window.location.replace("galery/");
+function OpenGame () {
+  const _0x5533fc = _0x2cdba3
+  klik[_0x5533fc(0x171)](),
+    VanzMenu[_0x5533fc(0x14a)][_0x5533fc(0x164)](_0x5533fc(0x158)),
+    VanzGame[_0x5533fc(0x14a)][_0x5533fc(0x155)](_0x5533fc(0x14e))
 }
-// Link
-
-function OpenSpotify() {
-  klik.play();
-  window.location.replace("https://open.spotify.com/playlist/1ZaVZkzQxjsSPUSnLDrL27?si=8580e4c5b3be448d");
+function OpenGalery () {
+  const _0x637ab3 = _0x2cdba3
+  klik['play'](), window[_0x637ab3(0x15d)]['replace'](_0x637ab3(0x172))
 }
-
-var klik = new Audio("https://editor-miring-squad.netlify.app/am/mix/klik.mp3");
-klik.loop = false;
-klik.autoplay = false;
-
-function OpenIg() {
-  klik.play();
-  window.location.replace("https://instagram.com/marchell_kevandra");
+function OpenSpotify () {
+  const _0x1cd53e = _0x2cdba3
+  klik['play'](), window[_0x1cd53e(0x15d)][_0x1cd53e(0x139)](_0x1cd53e(0x14f))
 }
-function OpenSteam() {
-  klik.play();
-  window.location.replace("https://steamcommunity.com/profiles/76561198858660119/");
+var klik = new Audio(_0x2cdba3(0x177))
+function _0x4a95 (_0x5e9a5a, _0x2ed05c) {
+  const _0x2f5062 = _0x2f50()
+  return (
+    (_0x4a95 = function (_0x4a9515, _0x546591) {
+      _0x4a9515 = _0x4a9515 - 0x137
+      let _0x4d3cbd = _0x2f5062[_0x4a9515]
+      return _0x4d3cbd
+    }),
+    _0x4a95(_0x5e9a5a, _0x2ed05c)
+  )
 }
-function OpenFb() {
-  klik.play();
-  window.location.replace("https://facebook.com/chellgnzxz");
+;(klik[_0x2cdba3(0x149)] = ![]), (klik[_0x2cdba3(0x13d)] = ![])
+var BacksoundMusic = new Audio(_0x2cdba3(0x165))
+;(BacksoundMusic['loop'] = ![]), (BacksoundMusic[_0x2cdba3(0x13d)] = ![])
+function OpenIg () {
+  const _0x3f28b4 = _0x2cdba3
+  klik[_0x3f28b4(0x171)](),
+    window[_0x3f28b4(0x15d)][_0x3f28b4(0x139)](_0x3f28b4(0x169))
 }
-function OpenTiktok() {
-  klik.play();
-  window.location.replace("https://tiktok.com/@mkxchl");
+function OpenSteam () {
+  const _0x9d4f0c = _0x2cdba3
+  klik[_0x9d4f0c(0x171)](),
+    window['location'][_0x9d4f0c(0x139)](_0x9d4f0c(0x16c))
 }
-function OpenProfilSpotify() {
-  klik.play();
-  window.location.replace("https://open.spotify.com/user/31ubcol5qj4y6mu3ifoxqbjgzpma?si=c48b3b26de694129");
+function OpenFb () {
+  const _0x242953 = _0x2cdba3
+  klik[_0x242953(0x171)](),
+    window[_0x242953(0x15d)][_0x242953(0x139)](
+      'https://facebook.com/chellgnzxz'
+    )
 }
+function OpenTiktok () {
+  const _0x391900 = _0x2cdba3
+  klik['play'](), window[_0x391900(0x15d)]['replace'](_0x391900(0x14d))
+}
+function OpenProfilSpotify () {
+  const _0x283e2e = _0x2cdba3
+  klik[_0x283e2e(0x171)](),
+    window[_0x283e2e(0x15d)][_0x283e2e(0x139)](_0x283e2e(0x148))
+}
+function VanzXLoad () {
+  setTimeout(function () {
+    const _0x186d23 = _0x4a95
+    VanzXNavMusic[_0x186d23(0x14a)]['add'](_0x186d23(0x16d)),
+      (IconMusic['className'] = MuteVolume)
+  }, 0x3e8)
+}
+function PlayMusic () {
+  const _0xaf1dec = _0x2cdba3
+  ;(IconMusic[_0xaf1dec(0x170)] = FullVolume),
+    BacksoundMusic[_0xaf1dec(0x171)](),
+    VanzXNavMusic[_0xaf1dec(0x14a)][_0xaf1dec(0x164)](_0xaf1dec(0x16d)),
+    BodyX[_0xaf1dec(0x14a)][_0xaf1dec(0x155)](_0xaf1dec(0x162)),
+    snowflakesSnow['classList'][_0xaf1dec(0x155)](_0xaf1dec(0x168))
+}
+snowflakes['forEach'](_0x348a2b => {
+  const _0x5cf822 = _0x2cdba3,
+    _0x4bc1a7 = Math[_0x5cf822(0x174)]() * 0x64,
+    _0x2258ea = Math[_0x5cf822(0x174)]() * 0x3 + 0x2,
+    _0x55ad9d = Math[_0x5cf822(0x174)]() * 0x5
+  ;(_0x348a2b['style'][_0x5cf822(0x13b)] = _0x4bc1a7 + 'vw'),
+    (_0x348a2b[_0x5cf822(0x15b)][_0x5cf822(0x176)] = _0x2258ea + 's'),
+    (_0x348a2b[_0x5cf822(0x15b)][_0x5cf822(0x166)] = _0x55ad9d + 's')
+})
